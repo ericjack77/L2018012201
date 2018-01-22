d3891e7b8683d2b8d9cd8dbf68bfba2946b485af
ericchiu <ericjack7@gmail.com>
2018-01-22 10:34:31 +0800

    解讀檔案裡的 .db檔

--- app/src/main/java/com/example/student/l2018012201/MainActivity.java
+++ app/src/main/java/com/example/student/l2018012201/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.student.l2018012201;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import java.io.File;
@@ -21,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void click1(View v)
     {
-        File filedb = new File(getFilesDir(),"Student.db");
+        File filedb = new File(getFilesDir(),"student.db");
         InputStream is = getResources().openRawResource(R.raw.student);
         try {
             OutputStream os =new FileOutputStream(filedb);
@@ -37,5 +40,17 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void click2(View v)
+    {
+        File dbFile = new File(getFilesDir(),"student.db");
+        SQLiteDatabase sd =SQLiteDatabase.openDatabase(dbFile.getAbsolutePath(),null,SQLiteDatabase.OPEN_READWRITE);
+        Cursor c = sd.rawQuery("Select * from students",null);
+        c.moveToFirst();
+        Log.d("DB",c.getString(1)+","+c.getString(2));
+        c.moveToNext();
+        Log.d("DB",c.getString(1)+","+c.getString(2));
+
     }
 }
